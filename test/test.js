27b194ad046ab2ba9df909f4bafcf106d0a1007f
Zorg-Moris <mvr.leon@gmail.com>
2020-04-06 20:00:13 +0300

    structure project

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const { performance } = require('perf_hooks');
+const assert = require('assert');
+const { binarySearchTree } = require('../src/index');
+const { first, second, third } = require('./strings');
+describe('Search Substring', function() {
+  it('Test case First should return true', function() {
+    let time = performance.now();
+    const result = searchStr(first);
+    console.log(performance.now() - time);
+    assert.equal(result, true)
+  });
+  it('Test case Second should return false', function() {
+    let time = performance.now();
+    const result = searchStr(second);
+    console.log(performance.now() - time);
+    assert.equal(result, false)
+  });
+  it('Test case Third should return true', function() {
+    let time = performance.now();
+    const result = searchStr(third);
+    console.log(performance.now() - time);
+    assert.equal(result, true)
+  });
+});
